3f23300d361d59eebb6ba61c50c1d14599da361b
steelcheng1221 <steelcheng@163.com>
2024-01-14 06:33:55 +0000

    增加 CSRF 验证

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
 Route::resource('/users','UsersController');
-Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+
 /*
 Route::get('/', function () {
     return view('welcome');
